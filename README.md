3ff956a0847046643887a13aa07185896202e1fe
abhisanGhimire <abhisanghimire@dr-ait.org>
2020-09-21 00:25:41 +0545

    readme_edit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Contact Form Package
 
+<img src="https://img.shields.io/github/issues/abhisanGhimire/package_development_for_laravel?style=for-the-badge">
+
 ## Sends email to admin and save in database, it uses bulma CSS for front-end.
--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -13,7 +13,9 @@ class ContactServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__ . '/views', 'contact');
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
         $this->mergeConfigFrom(__DIR__ . '/config/contact.php', 'contact');
-        $this->publishes([__DIR__ . '/config/contact.php' => config_path('contact.php')]);
+        $this->publishes([__DIR__ . '/config/contact.php' => config_path('contact.php'),
+            __DIR__ . '/views' => resource_path('views/vendor/contact'),
+        ]);
     }
     public function register()
     {
