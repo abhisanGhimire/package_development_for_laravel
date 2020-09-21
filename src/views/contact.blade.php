5f1322c77ea9887bdbcc7c4b7e2fb5b5bbeb1594
abhisanGhimire <abhisanghimire@dr-ait.org>
2020-09-21 11:10:17 +0545

    icon_added

--- src/views/contact.blade.php
+++ src/views/contact.blade.php
@@ -9,6 +9,7 @@
         <meta name="description" content="" />
         {{-- Bulma CSS CDN --}}
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
 
     <body>
